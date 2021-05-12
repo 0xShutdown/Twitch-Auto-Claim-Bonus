52ff48dc804bd6e8400a689b74183beba671de8f
Francisco Silva <13328492+SHUTDOWN45@users.noreply.github.com>
2021-05-12 09:13:09 +0100

    Update twitch-auto-claim-bonus.user.js

--- twitch-auto-claim-bonus.user.js
+++ twitch-auto-claim-bonus.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
-// @name Twitch Auto Claim Bonus
-// @icon  https://i.imgur.com/bgeDZWC.png
-// @version 1.0
-// @author SHUTDOWN
-// @description Automatically claim twitch channel points.
-// @match https://www.twitch.tv/*
-// @match https://dashboard.twitch.tv/*
-// @license MIT
+// @name          Twitch Auto Claim Bonus
+// @icon          https://raw.githubusercontent.com/SHUTDOWN45/Twitch-Auto-Claim-Bonus/main/icon.png
+// @description   Automatically claim twitch channel points.
+// @author        SHUTDOWN
+// @copyright     2021, SHUTDOWN
+// @version       1.0
+// @license       MIT
+// @match         https://www.twitch.tv/*
+// @match         https://dashboard.twitch.tv/*
+// @homepage      https://github.com/SHUTDOWN45/Twitch-Auto-Claim-Bonus
+// @supportURL    https://github.com/SHUTDOWN45/Twitch-Auto-Claim-Bonus/issues
+// @updateURL     https://raw.githubusercontent.com/SHUTDOWN45/Twitch-Auto-Claim-Bonus/master/twitch-auto-claim-bonus.user.js
+// @downloadURL   https://raw.githubusercontent.com/SHUTDOWN45/Twitch-Auto-Claim-Bonus/master/twitch-auto-claim-bonus.user.js
 // @grant none
 // ==/UserScript==
 
